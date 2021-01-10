5e7e28f321e46cc82e9fe9655d14fa17e5a97cf0
Max <zhdanom.maksi@gmail.com>
2021-01-10 22:44:40 +0300

    Initial commit

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'tours/static')
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
