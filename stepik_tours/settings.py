502c6596dc942987f9b685129762c7d956af58cb
Max <zhdanom.maksi@gmail.com>
2021-02-03 21:55:35 +0300

    Initial commit

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -99,15 +99,16 @@ AWS_ACCESS_KEY_ID = 'AKIAXVRV4FYX6YOSIZ4T'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'stepik-static'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
 AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tours/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'stepik_tours.storage_backends.MediaStorage'
 
 MEDIA_URL = '/media/'
